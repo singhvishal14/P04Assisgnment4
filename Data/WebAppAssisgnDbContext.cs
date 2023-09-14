124204f31ade5e3a2aa20b431bf09580da466084
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-09-14 22:38:19 +0530

    first commit

---
+++ Data/WebAppAssisgnDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAssisgn.Models;
+
+namespace WebAppAssisgn.Data
+{
+    public class WebAppAssisgnDbContext : DbContext
+    {
+        public WebAppAssisgnDbContext (DbContextOptions<WebAppAssisgnDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAssisgn.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppAssisgn.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public int Age { get; set; }
+
+    }
+}
